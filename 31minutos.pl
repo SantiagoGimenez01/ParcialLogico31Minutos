ba6bec89d1696d43d0861985cdeb6ddc64acefd0
SantiagoGimenez01 <sgimenezburgos@frba.utn.edu.ar>
2024-08-14 13:43:17 -0300

    Puntos 3, 4, 5 y 6

--- 31minutos.pl
+++ 31minutos.pl
@@ -45,11 +45,20 @@ noEsReconocidaPorCriticos(Cancion):-
 
 % Punto 3
 % Saber si dos compositores son colaboradores, lo cual ocurre si compusieron alguna canción juntos.
+sonColaboradores(UnCompositor, OtroCompositor):-
+    cancion(_, Compositores, _),
+    member(UnCompositor, Compositores),
+    member(OtroCompositor, Compositores),
+    UnCompositor \= OtroCompositor.
 
 % En el noticiero 31 Minutos cada trabajador puede tener múltiples trabajos. Algunos de los tipos de trabajos que existen son:
 % ● Los conductores, de los cuales nos interesa sus años de experiencia.
 % ● Los periodistas, de los cuales nos interesa sus años de experiencia y su título, el cual puede ser licenciatura o posgrado.
 % ● Los reporteros, de los cuales nos interesa sus años de experiencia y la cantidad de notas que hicieron a lo largo de su carrera.
+
+% conductor(Experiencia)
+% periodista(Experiencia, Titulo),
+% reportero(Experiencia, Notas).
 
 % Punto 4
 % Modelar en la solución a los siguientes trabajadores:
@@ -57,6 +66,13 @@ noEsReconocidaPorCriticos(Cancion):-
 % b. Bodoque, periodista con 2 años de experiencia con un título de licenciatura, y también reportero con 5 años de experiencia y 300 notas realizadas.
 % c. Mario Hugo, periodista con 10 años de experiencia con un posgrado.
 % d. Juanin, que es un conductor que recién empieza así que no tiene años de experiencia.
+
+% esTrabajador(Nombre, Profesion)
+esTrabajador(tulio, conductor(5)).
+esTrabajador(bodoque, periodista(2, licenciatura)).
+esTrabajador(bodoque, reportero(5, 300)).
+esTrabajador(marioHugo, periodista(10, posgrado)).
+esTrabajador(juanin, conductor(0)).
 
 % Punto 5
 % Conocer el sueldo total de una persona, el cual está dado por la suma de los sueldos de cada uno de sus trabajos. 
@@ -66,7 +82,30 @@ noEsReconocidaPorCriticos(Cancion):-
 % c. Los periodistas, por cada año de experiencia reciben 5000, pero se les aplica un porcentaje de incremento del 20% cuando tienen una licenciatura o 
 % del 35% si tienen un posgrado.
 
+sueldoTrabajador(Persona, SueldoTotal):-
+    esTrabajador(Persona, _),
+    findall(Sueldo, (esTrabajador(Persona, Trabajo), sueldoPorTrabajo(Trabajo, Sueldo)), Sueldos),
+    sumlist(Sueldos, SueldoTotal).
+        
+sueldoPorTrabajo(conductor(Experiencia), Sueldo):-
+    Sueldo is (Experiencia * 10000).
+sueldoPorTrabajo(reportero(Experiencia, Notas), Sueldo):-
+    Sueldo is (Experiencia * 10000 + Notas * 100).
+sueldoPorTrabajo(periodista(Experiencia, licenciatura), Sueldo):-
+    Sueldo is (Experiencia * 5000 * 1.2).
+sueldoPorTrabajo(periodista(Experiencia, posgrado), Sueldo):-
+    Sueldo is (Experiencia * 5000 * 1.35).
+
 % Punto 6
 % Agregar un nuevo trabajador que tenga otro tipo de trabajo nuevo distinto a los anteriores. Agregar una forma de calcular el sueldo para el nuevo 
 % trabajo agregado
+% camarografo(Experiencia, NotasFilmadas) -> El sueldo se calcula multiplicando los años de experiencia por las notasFilmadas y sumando 10000 a ese monto
+esTrabajador(santiago, camarografo(8, 350)).
+sueldoPorTrabajo(camarografo(Experiencia, Filmaciones), Sueldo):-
+    Sueldo is Experiencia * Filmaciones + 10000.
+
 % ¿Qué concepto de la materia se puede relacionar a esto?
+
+% Puedo relacionarlo con el concepto de escalabilidad, el cual permite que el codigo pueda escalar, es decir se puedan agregar cosas sin tener que
+% cambiar muchas cosas, manteniendo las funciones creadas previamente, a mayor escalabilidad mas facil va a ser agregar informacion sin tener que cambiar
+% cosas hechas antes de los agregados
